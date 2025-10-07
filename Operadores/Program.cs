a1d9fad8055e03ed2b898e93765a6d9d80a8db30
Lucavinini <lucas.00000850855@unicap.br>
2025-10-07 19:06:51 -0300

    Adiciona projeto de Operadores Aritmédicos

---
+++ Operadores/Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+
+        //Operadores Aritmédicos
+        Console.WriteLine(12 + 22);
+        Console.WriteLine(2 - 2);
+        Console.WriteLine(2 * 2);
+        //Potenciação
+        //Não existe nativamente, apenas com o uso de bibliotecas.
+        Console.WriteLine(2 / 2);
+        Console.WriteLine(2 % 4);
+
+
+
+        //Operadores de Atribuição
+
+        // (=) Atribuição simples
+
+        int x = 30;
+
+        x = 40;
+
+        // Incremento
+        x++;
+        Console.WriteLine(x);
+
+        //Atribuição composta, segue-se da operação que vai feito com um igual e um valor.
+
+        x -= 1;
+        Console.WriteLine(x);
+        
+        x--;
+
+        Console.WriteLine(x);
+
+
+    }
+}
