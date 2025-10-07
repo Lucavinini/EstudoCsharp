abb75a774a1c5ddee7931ce76b6e8822b3d10283
Lucavinini <lucas.00000850855@unicap.br>
2025-10-07 17:55:50 -0300

    Adiciona projeto de I/O

---
+++ InputOutput/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EntradaESaida;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("---CADASTRO----");
+        Console.WriteLine("Digite o seu nome:");
+
+        string Nome_Usuario = Console.ReadLine(); //Principal comando para receber dados
+        Console.WriteLine("Digite a sua idade:");
+
+        int Idade_Usuario = Convert.ToInt32(Console.ReadLine()); //Uso do Convert.ToTipoDeDado() para converter tipo string para outro tipo.
+
+        Console.WriteLine($"Bem vindo {Nome_Usuario}, sua idade é: {Idade_Usuario}");
+
+    }
+}
