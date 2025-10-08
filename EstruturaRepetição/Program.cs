0a75575a9edc16351084894be4780c1ffc253771
Lucavinini <lucas.00000850855@unicap.br>
2025-10-08 14:30:25 -0300

    Adiciona projeto "Estrutura de Repetição" com implementação de laços while e for

---
+++ EstruturaRepetição/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Laço;
+
+public class Program
+{
+    public static void Main()
+    {
+        //While
+        int passos = 30;
+
+        while (passos <= 30)
+        {
+            Console.WriteLine(passos);
+            passos++;
+        }
+
+        //for, utiliza-se os ; para separar os campos.
+        int i = 30;
+
+        for (; i > 0;)
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+
+    }
+}
