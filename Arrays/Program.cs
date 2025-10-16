f5bdf994b085f0dd32b7fbb05f0ce6c32f064787
Lucavinini <lucas.00000850855@unicap.br>
2025-10-16 17:40:13 -0300

    Adiciona projeto "Arrays" com implementação de manipulação de arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        //1.Declaração de um array inicializado
+
+        //Array sempre terá um tipo.
+
+        string[] produtos = { "Martelo", "Fita", "Machado" };
+
+        //2. Acessando valores por meio de indice
+        Console.WriteLine(produtos[0]);
+
+        //Acessando por meio de loops
+        foreach (string produto in produtos)
+        {
+            Console.WriteLine(produto);
+        }
+
+        //3. Declaração sem inicializar
+
+        string[] amigos = new string[5];
+        
+        for (int cont = 0; cont < 5; cont++)
+        {
+            Console.WriteLine("Digite uma palavra:");
+            amigos[cont] = Console.ReadLine();
+        }
+
+        for (int cont = 0; cont < 5; cont++)
+        {
+            Console.WriteLine(amigos[cont]);
+        }
+
+    }
+}
